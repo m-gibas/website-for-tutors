9c88d2f0994f17249ed00f5b1e3acfa5e468942d
Maksymilian Gibas <104219943+m-gibas@users.noreply.github.com>
2023-01-25 00:19:34 +0100

    register/login page update

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Tile from './components/Tile';
 import Tutors from './components/Tutors';
 import Register from './components/Register';
-import Login from './components/Login';
 
 function App() {
 
@@ -13,15 +12,13 @@ function App() {
       <Routes>
         <Route path='/' element={
           <div className="container">
-            <Link to="/register" className="mainpage-link"><Tile text='Register' /></Link>
-            <Link to="/login" className="mainpage-link"><Tile text='Log in' /></Link>
+            <Link to="/register" className="mainpage-link"><Tile text='Log in' /></Link>
             <Link to="/tutors" className="mainpage-link"><Tile text='Tutors' /></Link>
             <Link to="/about" className="mainpage-link"><Tile text='About' /></Link>
             <Link to="/about" className="mainpage-link"><Tile text='About' /></Link>
           </div>
         }/>
         <Route path='/register' element={ <Register /> } />
-        <Route path='/login' element={ <Login /> } />
         <Route path='/tutors' element={ <Tutors />} />
         <Route path='/about' element={ <p>About page <Link to="/">Main Page</Link></p> } />
       </Routes>
--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -6,42 +6,41 @@ const Register = () => {
     const [isFlipped, setIsFlipped] = useState(false)
 
   return (
-    // <div className='register-container' >
     <div className='register-container' >
 
     <div className={`card ${isFlipped ? 'is-flipped' : ''}`}>
 
-        <form className={`register-form ${isFlipped ? 'register-form--back' : ''}`}>
-            <h3>Registration form:</h3>
+        <form className='register-form register-form-front'>
+             <h3>Login form:</h3>
             <label htmlFor='email'>E-mail: </label>
-            <input type='text' id='email' name='email' placeholder="Type your e-mail" />
-
-            <label htmlFor='firstName'>First name: </label>
-            <input type='text' id='firstName' name='firstName'  placeholder="Type your first name" />
-
-            <label htmlFor='lastName'>Last name: </label>
-            <input type='text' id='lastName' name='lastName' placeholder="Type your last name" />
+            <input type='text' id='login-email' name='email' placeholder="Type your e-mail" />
 
             <label htmlFor='password'>Password: </label>
-            <input type='password' id='password' name='password'  placeholder="Type your password" />
+            <input type='password' id='login-password' name='password'  placeholder="Type your password" />
 
-            <input type='submit' value='Register' className='submit-button' />
+            <input type='submit' value='Log in' className='submit-button' />
         </form>
 
-        <form className={`register-form ${isFlipped ? '' : 'register-form--back'}`}>
-            <h3>Login form:</h3>
+        <form className='register-form register-form-back'>
+            <h3>Registration form:</h3>
             <label htmlFor='email'>E-mail: </label>
-            <input type='text' id='email' name='email' placeholder="Type your e-mail" />
+            <input type='text' id='register-email' name='email' placeholder="Type your e-mail" />
+
+            <label htmlFor='firstName'>First name: </label>
+            <input type='text' id='register-firstName' name='firstName'  placeholder="Type your first name" />
+
+            <label htmlFor='lastName'>Last name: </label>
+            <input type='text' id='register-lastName' name='lastName' placeholder="Type your last name" />
 
             <label htmlFor='password'>Password: </label>
-            <input type='password' id='password' name='password'  placeholder="Type your password" />
+            <input type='password' id='register-password' name='password'  placeholder="Type your password" />
 
             <input type='submit' value='Register' className='submit-button' />
         </form>
 
     </div>
 
-    <button className='submit-button' onClick={() => setIsFlipped(!isFlipped)}>Switch to {isFlipped ? 'Register' : 'Login'}</button>
+    <button className='switch-button' onClick={() => setIsFlipped(!isFlipped)}>Switch to {isFlipped ? 'Log in' : 'Register'}</button>
     <Link to="/" className="link">Main Page</Link>
 
     </div>
