68a8c2df67de1fcb4b775f1b8a362cc95b5324cb
Maksymilian Gibas <104219943+m-gibas@users.noreply.github.com>
2023-01-29 23:24:44 +0100

    small about-tutor layout update

--- src/components/AboutTutor/AboutTutor.tsx
+++ src/components/AboutTutor/AboutTutor.tsx
@@ -13,16 +13,25 @@ const AboutTutor = ({ tutors }: TutorsProps) => {
     <>
       <GoBackButton />
       <div className="about-tutor">
-          <img src={`/Images/${tutor.imgPath}`} alt="person"></img>
-          <p>{`${tutor.name} ${tutor.surname}`}</p>
-          <span>{tutor.short_desc}</span>
+        <div className="about-tutor-card">
+          <div className='about-tutor-img'>
+              <img src={`/Images/${tutor.imgPath}`} alt="person"></img>
+          </div>
+          <div className="about-tutor-details">
+            <p>{`${tutor.name} ${tutor.surname}`}</p>
+            <span>{tutor.short_desc}</span> <br />
+            <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto eveniet culpa quas accusamus facilis, nam perferendis voluptatum ex aperiam harum animi veritatis. Vel quo accusantium architecto perspiciatis ducimus ad aliquid porro aut, deserunt delectus illum dolore, eum laudantium maiores ea!</span>
+          </div>
+        </div>
 
           <Link to="/tutors" className="link">Tutors Page</Link>
       </div>
     </>
     : <>
-        <p>There is no tutor here with that id!</p>
         <GoBackButton />
+        <div className="tutors-container">
+          <h3>There is no tutor here with that id!</h3>
+        </div>
     </>
 
     
